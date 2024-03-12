deeb062b5b9296bb8299213ccfb57019f8b2e5c0
Job <job.santos924@gmail.com>
2024-03-12 21:15:27 +1300

    feat: prototype art and more polished movement script

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -4,55 +4,167 @@ using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
-    [Header("Movement Settings")]
-    [SerializeField] private float movementSpeed = 5f;
-    [SerializeField] private float jumpForce = 5f;
+    [Header("Movement Configuration")]
+    public float moveSpeed = 5f;
+    public float groundRayCheckDistance = 0.2f;
+    [Header("Jump Configuration")]
+    [Range(1, 20)]
+    public float jumpVelocity;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public float playerDownFallMultiplier = 4f; // this is the multiplier for when the player presses down while in the air
+
     [Header("References")]
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Collider2D characterEntityCollider;
 
-    // ======= VARIABLES ========
-    #region VARIABLES
-    private Vector2 movementInput = Vector2.zero;
-    private bool jumpInput = false;
+    #region Private Variables
+    #region Input Variables
+    private float horizontalInput;
+    private float verticalInput;
+    private bool jumpInput;
+    private bool hasPressedDown = false; // player has pressed down while in the air
+    private bool disableMovementInput = false;
+    private bool disableJumpInput = false;
+    #endregion
+
+    #region Physics Variables
+    private float jumpVelocityCalculation = 0;
+    private float fallVelocityCalculation = 0;
+    private float playerDownFallVelocityCalculation = 0;
+    #endregion
+
+    #region Check State Variables
+    private bool isGrounded;
+    #endregion
+
     #endregion
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        if (GetComponent<Rigidbody2D>() == null) {
+            gameObject.AddComponent<Rigidbody2D>();
+        } 
+        else {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        if (GetComponent<Collider2D>() == null) {
+        } 
+        else 
+        {
+        characterEntityCollider = GetComponent<Collider2D>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetInputs();
+        GetInput();
+        jumpVelocityCalculation = Physics2D.gravity.y * (fallMultiplier - 1);
+        fallVelocityCalculation = Physics2D.gravity.y * (lowJumpMultiplier - 1);
+        playerDownFallVelocityCalculation = Physics2D.gravity.y * (playerDownFallMultiplier - 1);
     }
 
-    private void GetInputs()
-    {
-        movementInput.x = Input.GetAxisRaw("Horizontal");
-        movementInput.y = Input.GetAxisRaw("Vertical");
-        jumpInput = Input.GetButton("Jump");
-    }
-
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         Move();
         Jump();
+
+        JumpVelocityCalculation();
     }
 
     private void Move()
     {
-        Vector2 direction = new Vector2(movementInput.x, movementInput.y);
-        rb.velocity = (direction.normalized * movementSpeed);
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+        FlipPlayer();
     }
 
     private void Jump()
     {
-        if (jumpInput)
+        if (jumpInput && isGrounded)
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            rb.velocity += Vector2.up * jumpVelocity;
         }
     }
 
+    private void JumpVelocityCalculation()
+    {
+        CheckIfGrounded();
+
+        // if the player presses down before they have reached the peak of their jump, they will fall faster
+        if (verticalInput < -0.3 && !isGrounded)
+        {
+            hasPressedDown = true;
+        }
+        
+        if (hasPressedDown && !isGrounded) 
+        {
+            rb.velocity += playerDownFallVelocityCalculation * Time.deltaTime * Vector2.up;
+        } 
+        else if (rb.velocity.y > 0 && !jumpInput)
+        {
+            rb.velocity += fallVelocityCalculation * Time.deltaTime * Vector2.up;
+        }
+        else if (rb.velocity.y < 0) 
+        {
+            rb.velocity += jumpVelocityCalculation * Time.deltaTime * Vector2.up;
+        } 
+    }
+
+    private bool CheckIfGrounded()
+    {
+        Ray ray = new Ray(transform.position, Vector2.down);
+
+        // debug draw line of ground ray
+        Debug.DrawRay(ray.origin, ray.direction * (characterEntityCollider.bounds.extents.y + groundRayCheckDistance), Color.red);
+
+        if (Physics2D.Raycast(rb.position, Vector2.down, characterEntityCollider.bounds.extents.y + groundRayCheckDistance, LayerMask.GetMask("Ground")))
+        {
+            isGrounded = true;
+            hasPressedDown = false;
+            return true;
+        }
+        else
+        {
+            isGrounded = false;
+            return false;
+        }
+    }
+
+    public bool IsPlayerGrounded()
+    {
+        return isGrounded;
+    }
+
+    private void FlipPlayer()
+    {
+        // rotate players -180 if pressing left
+        if (horizontalInput < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, -180, 0);
+        }
+        else if (horizontalInput > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+
+    public void ToggleMovementAbility(bool isActive)
+    {
+        disableMovementInput = isActive;
+    }
+    
+    public void ToggleJumpAbility(bool isActive)
+    {
+        disableJumpInput = isActive;
+    }
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+        jumpInput = Input.GetButton("Jump");
+    }
 }
