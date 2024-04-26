d296e19654c5c707efc2ae2d7a3563f47fecdd42
Job <job.santos924@gmail.com>
2024-04-26 16:24:47 +1200

    feat: parallax on cheeto world

---
+++ Assets/Scripts/Background/ParallaxCamera.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+ 
+// https://www.youtube.com/watch?v=MEy-kIGE-lI
+[ExecuteInEditMode]
+public class ParallaxCamera : MonoBehaviour
+{
+    public delegate void ParallaxCameraDelegate(float deltaMovement);
+    public ParallaxCameraDelegate onCameraTranslate;
+ 
+    private float oldPosition;
+ 
+    void Start()
+    {
+        oldPosition = transform.position.x;
+    }
+ 
+    void Update()
+    {
+        if (transform.position.x != oldPosition)
+        {
+            if (onCameraTranslate != null)
+            {
+                float delta = oldPosition - transform.position.x;
+                onCameraTranslate(delta);
+            }
+ 
+            oldPosition = transform.position.x;
+        }
+    }
+}
